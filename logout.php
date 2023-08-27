ab64f2db45039b83f8a21fa63bedbd2f60abfe5e
PhilipsTruong <phuctinvg9999@gmail.com>
2023-08-28 01:09:49 +0700

    logout

--- logout.php
+++ logout.php
@@ -7,6 +7,6 @@ unset($_SESSION['ADMIN_FULLNAME']);
 unset($_SESSION['ADMIN_USERNAME']);  
 unset($_SESSION['ADMIN_ROLE']); 
  
- unset($_SESSION['admin_gcCart']);
+unset($_SESSION['admin_gcCart']);
 redirect("login.php?logout=1");
 ?>
